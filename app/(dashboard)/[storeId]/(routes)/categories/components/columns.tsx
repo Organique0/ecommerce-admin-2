4f2d7ae73e4bec7efc4d1a73ad04e8e4abd58211
Organique0 <grabnar.luka@gmail.com>
2023-08-08 21:57:45 +0200

    view orders page

--- app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { ArrowUpDown } from "lucide-react"
 import CellAction from "./CellAction";
 
 // This type is used to define the shape of our data.
