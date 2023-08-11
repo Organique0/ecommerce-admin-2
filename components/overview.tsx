62e6d3bbf97a98c81447c8acb5b017c9cd4c21ca
Organique0 <grabnar.luka@gmail.com>
2023-08-11 22:40:59 +0200

    revenue graph

---
+++ components/overview.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
+
+interface OverviewProps {
+    data: any[];
+}
+
+const Overview: React.FC<OverviewProps> = ({ data }) => {
+    return (
+        <ResponsiveContainer width="100%" height={350}>
+            <BarChart data={data}>
+
+                <XAxis dataKey="name" stroke="#000000" fontSize={15} tickLine={true} axisLine={false} />
+
+                <YAxis stroke="#000000" fontSize={15} tickLine={false} axisLine={false} tickFormatter={(value) => `$${value}`} />
+
+                <Bar dataKey="total" fill="#741f91" radius={[4, 4, 0, 0]} />
+            </BarChart>
+        </ResponsiveContainer>
+    );
+}
+
+export default Overview;
