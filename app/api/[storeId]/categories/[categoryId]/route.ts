52a663c9e1a2a458681b2cea1fd40728b572e078
Organique0 <grabnar.luka@gmail.com>
2023-08-11 19:03:42 +0200

    stripe

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -11,7 +11,10 @@ export async function GET(req:Request,{params}:{params:{categoryId:string}}) {
         const category = await prismadb.category.findUnique({
             where:{
                 id:params.categoryId,
-            }
+            },
+            include: {
+                billboard:true
+            },
         });
 
         return NextResponse.json(category);
